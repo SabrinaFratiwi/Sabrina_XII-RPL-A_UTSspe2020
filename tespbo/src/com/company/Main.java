809cd96581c8ef1c2b97f883feaf09916bb1ee0b
Sabrina <70954776+SabrinaFratiwi@users.noreply.github.com>
2020-09-09 06:30:13 +0700

    Add files via upload

---
+++ tespbo/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println ("Nama Saya " + str. charAt(18) + str.charAt(0) + str. charAt(1) + str. charAt(17)
+                + str. charAt(8) + str. charAt(13) + str. charAt(0));
+    }
+}
